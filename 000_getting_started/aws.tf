8cc2d265988b859c908f16eaf3d57862a006498b
johnitopaisah <johnitopaisah@gmail.com>
2025-03-04 19:52:04 +0100

    updated the main folder

--- 000_getting_started/aws.tf
+++ 000_getting_started/aws.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "my_server" {
-  ami           = "ami-087c17d1fe0178315"
+resource "aws_instance" "app_server" {
+  ami           = "ami-02a53b0d62d37a757"
   instance_type = var.instance_type
 
   tags = {
@@ -7,7 +7,6 @@ resource "aws_instance" "my_server" {
   }
 }
 
-/*
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   providers = {
@@ -29,4 +28,3 @@ module "vpc" {
     Environment = "dev"
   }
 }
-*/
--- 000_getting_started/main.tf
+++ 000_getting_started/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.89.0"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-02a53b0d62d37a757"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "MyServer"
-  }
+locals {
+  project_name = "JohnItopa"
 }
