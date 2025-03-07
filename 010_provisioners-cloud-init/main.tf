92e22412bfdc78c0c3955204fa9304846f0b3fd2
johnitopaisah <johnitopaisah@gmail.com>
2025-03-07 11:34:02 +0100

    added the main.tf file

--- 010_provisioners-cloud-init/main.tf
+++ 010_provisioners-cloud-init/main.tf
@@ -1,17 +1,17 @@
 terraform {
-	/*
-  backend "remote" {
-    organization = "ExamPro"
 
-    workspaces {
-      name = "provisioners"
-    }
-  }
-	*/
+  # backend "remote" {
+  #   organization = "ExamPro"
+
+  #   workspaces {
+  #     name = "provisioners"
+  #   }
+  # }
+	
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.59.0"
+      version = "~> 5.0"
     }
   }
 }
